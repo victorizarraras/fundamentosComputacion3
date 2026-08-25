d1e78d5c844c647d7f2d76c7473eaf398b7fe1ee
Victor Izarraras <vicand202@gmail.com>
2026-08-25 12:51:46 -0700

    linked list recorrida

--- PruebaDS3_LnkLst.java
+++ PruebaDS3_LnkLst.java
@@ -1,55 +1,74 @@
-package com.mycompany.pruebads3_lnklst;/**
- *
- * @author AngelLopez Vigil
- * @date Aug, 17, 2026
- */
-class Student {
+package com.mycompany.pruebads3_lnklst;
+
+class Student{
     String name;
     int score;
     Student next;
-    
-    public Student(String name, int score) {
+    public Student(String name, int score){
         this.name = name;
-        this.next = null;
         this.score = score;
+        this.next = null;
     }
 }
-    class LnkdLst {
-        Student head;
-        public LnkdLst() {
-    this.head = null;
+
+class LnkdLst {
+    Student head;
+    public LnkdLst(){  
+        //Constructor
+        this.head = null;
     }
-    //Agrega un nuevo estudiante al final de la lista
-    public void add(String name,int score) {
-        Student newStudent = new Student(name,score);
-        if(head == null) {
+    //Agrega un nuevo estudiante al final de la fila 
+    public void add (String name, int score){
+        Student newStudent = new Student(name, score);
+        if (head == null){
             head = newStudent;
-            return;
-      
-    }
+            return; 
+        }
         Student current = head;
-        while(current.next !=null) {
-           current = current.next;  
-        }   
+        while ( current.next != null){
+            current = current.next;
+        }
         current.next = newStudent;
     }
+    
     public void print() {
         Student current = this.head;
-        while(current !=null) {
+        while (current != null) {
             System.out.println(current.name + " - " + current.score);
             current = current.next;
         }
     }
-}
-
-public class PruebaDS3_LnkLst {
-    public static void main(String[] args) {
-        LnkdLst lista = new LnkdLst();
-        lista.add("Linus",90);
-        lista.add("ada", 95);
-        lista.add("Paul", 70);
-        lista.print();
+    
+    public Student retrieve(String searchedName){
+        Student current = this.head;
+        while (current != null){
+            if (current.name.equals(searchedName)){
+                return current;
+            }
+            current = current.next;
+        }
+        return null; 
     }
 }
-    
+
+
+
+public class PruebaDS3_LnkLst {
+    public static void main(String[] args) { 
+        LnkdLst lista = new LnkdLst();
+        lista.add( "Linus" , 90 );
+        lista.add( "Ada" , 95);
+        lista.add( "Paul", 78);
         
+        lista.print();
+        
+        //Student found = lista.retrieve("Ada");
+        //System.out.println("Found: "+ found.name+ "\nScore: "+found.score);
+        Student found = lista.retrieve("Ada");
+        System.out.println("Found: "+ found.name+ "\nScore: "+found.score);
+        found = lista.retrieve("Guillermo");
+        if(found != null){
+            System.out.println("Found: "+ found.name+ "\nScore: "+found.score);
+        }
+    }
+}
