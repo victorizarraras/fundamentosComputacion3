ca97fd58813263aa2cd0375d6529be0691cdeb42
Victor Izarraras <vicand202@gmail.com>
2026-09-03 12:43:20 -0700

    iteracion y cuenta de duplicados

--- NombresArrayList.java
+++ NombresArrayList.java
@@ -7,11 +7,14 @@ import java.util.List;
 public class NombresArrayList {
     public static void main(String[] args) {
         ArrayList<String> personalities = new ArrayList<>();
+        ArrayList<String> nombres = new ArrayList<>();
+        ArrayList<Integer> cantidadNombres = new ArrayList<>();
+
         personalities.add("Ada Lovelace");
         personalities.add("Alan turing");
         personalities.add("Grace hopper");
 
-        // --- Leer nombres desde el archivo de texto ---
+        // Leer nombre
         Path rutaArchivo = Path.of("listado.txt");
 
         try {
@@ -21,13 +24,27 @@ public class NombresArrayList {
             System.out.println("No se encontró el archivo: " + e.getMessage());
         }
 
-        String name = personalities.get(1);
-        System.out.println(name);
-        System.out.println("The size of the arraylist is: " + personalities.size());
+        for (int i = 0; i < personalities.size(); i++) {
+            // extraer solo la primera palabra del nombre completo
+            String[] partes = personalities.get(i).split(" ");
+            String primerNombre = partes[0];
 
-        personalities.size();
-        personalities.remove(0);
-        personalities.set(1, "Margaret Hamilton");
-        System.out.println(personalities);
+            // ya se vio el primer nombre antes?
+            int posicion = nombres.indexOf(primerNombre);
+
+            if (posicion == -1) {
+                // Si no existe se agrega
+                nombres.add(primerNombre);
+                cantidadNombres.add(1);
+            } else {
+                // Si ya existe se incrementa contador
+                int cantidadActual = cantidadNombres.get(posicion);
+                cantidadNombres.set(posicion, cantidadActual + 1);
+            }
+        }
+
+        for (int i = 0; i < nombres.size(); i++) {
+            System.out.println(nombres.get(i) + ": " + cantidadNombres.get(i));
+        }
     }
 }
